d4009626cf1a4122e2714038f380e2172c917d73
Orual <orual.nbc@proton.me>
2025-02-24 13:17:14 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "ESP32 Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+    outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-esp-dev,
+    flake-parts,
+    systems,
+    rust-overlay,
+    ...
+  }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem = { config, self', pkgs, lib, system, ... }: 
+      let 
+      esp-rs = pkgs.callPackage ./default.nix {
+        inherit pkgs;
+      };
+      nixpkgs-esp-dev = nixpkgs-esp-dev.legacyPackages.${system};
+      in {
+        packages.default = esp-rs;
+        devShells.default = pkgs.mkShell  {
+          name = "esp-rs";
+          inputsFrom = [ config.just-flake.outputs.devShell ];
+          buildInputs = with pkgs; [ 
+            esp-rs 
+            probe-rs
+            rustup 
+            espflash 
+            rust-analyzer
+            
+            pkg-config 
+            stdenv.cc 
+            bacon 
+            systemdMinimal 
+          ];
+
+          shellHook = ''
+          export PS1="(esp-rs)$PS1"
+          # this is important - it tells rustup where to find the esp toolchain,
+          # without needing to copy it into your local ~/.rustup/ folder.
+          export RUSTUP_TOOLCHAIN=${esp-rs}
+          '';
+      };
+    };
+  };
+}
---
+++ rust.nix
@@ -0,0 +1,46 @@
+{ 
+  pkgs,
+  archname ? "x86_64-unknown-linux-gnu"
+}:
+let
+in
+pkgs.stdenv.mkDerivation rec {
+    name = "esp-rust-build";
+    version = "1.84.0.0";
+
+    src = pkgs.fetchzip {
+            url = "https://github.com/esp-rs/rust-build/releases/download/v${version}/rust-${version}-${archname}.tar.xz";
+            sha256 = "sha256-U9JHQhHskj36bsiNvoAeGzz6sKvQ4bjla7zt1uys/Dg=";
+            # sha256 = "0000000000000000000000000000000000000000000000000000";
+          };
+
+    patchPhase = '' 
+    patchShebangs ./install.sh
+    '';
+
+    outputs = [ "out" ];
+
+    #Usage: ./install.sh [options]
+    #Options:
+
+    #    --uninstall                      only uninstall from the installation prefix
+    #    --destdir=[<none>]               set installation root
+    #    --prefix=[/usr/local]            set installation prefix
+    #    --without=[<none>]               comma-separated list of components to not install
+    #    --components=[<none>]            comma-separated list of components to install
+    #    --list-components                list available components
+    #    --sysconfdir=[/etc]              install system configuration files
+    #    --bindir=[/bin]                  install binaries
+    #    --libdir=[/lib]                  install libraries
+    #    --datadir=[/share]               install data
+    #    --mandir=[/share/man]            install man pages in PATH
+    #    --docdir=[\<default\>]           install documentation in PATH
+    #    --disable-ldconfig               don't run ldconfig after installation (Linux only)
+    #    --disable-verify                 don't obsolete
+    #    --verbose                        run with verbose output
+
+    installPhase = ''
+    mkdir -p $out
+    ./install.sh --destdir=$out --prefix="" --disable-ldconfig --without=rust-docs-json-preview,rust-docs
+    '';
+}
