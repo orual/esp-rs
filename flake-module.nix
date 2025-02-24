f85fd682fde45a723c0645b602a700555b0caa4f
Orual <orual.nbc@proton.me>
2025-02-24 13:56:10 -0500

    reworking to match just-flake module format

---
+++ flake-module.nix
@@ -0,0 +1,65 @@
+{
+  perSystem = { config, pkgs, lib, ... }: {
+    options =
+      let
+        inherit (lib) types;
+      in
+      {
+        esp-rs-flake = lib.mkOption {
+          default = { };
+          type = types.submoduleWith {
+            specialArgs = { inherit pkgs; };
+            modules = [{
+              imports = [
+                ./default.nix
+              ];
+
+              options = {
+
+
+                outputs.devShell = lib.mkOption {
+                  type = lib.types.package;
+                  readOnly = true;
+                  description = ''
+                    The output devShell to include in `inputsFrom`.
+                  '';
+                };
+              };
+            }];
+          };
+        };
+      };
+    config =
+      let
+        cfg = config.esp-rs-flake;
+      in
+      {
+        esp-rs-flake.outputs.devShell =
+          let
+            esp-rs = pkgs.callPackage ./default.nix {
+              inherit pkgs;
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [ 
+            esp-rs 
+            probe-rs
+            rustup 
+            espflash 
+            rust-analyzer
+            
+            pkg-config 
+            stdenv.cc 
+            bacon 
+            systemdMinimal 
+          ];
+            shellHook = ''
+          export PS1="(esp-rs)$PS1"
+          # this is important - it tells rustup where to find the esp toolchain,
+          # without needing to copy it into your local ~/.rustup/ folder.
+          export RUSTUP_TOOLCHAIN=${esp-rs}
+          '';
+          };
+      };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -12,47 +12,7 @@
     systems.url = "github:nix-systems/default";
   };
 
-    outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-esp-dev,
-    flake-parts,
-    systems,
-    rust-overlay,
-    ...
-  }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
-      perSystem = { config, self', pkgs, lib, system, ... }: 
-      let 
-      esp-rs = pkgs.callPackage ./default.nix {
-        inherit pkgs;
-      };
-      nixpkgs-esp-dev = nixpkgs-esp-dev.legacyPackages.${system};
-      in {
-        packages.default = esp-rs;
-        devShells.default = pkgs.mkShell  {
-          name = "esp-rs";
-          buildInputs = with pkgs; [ 
-            esp-rs 
-            probe-rs
-            rustup 
-            espflash 
-            rust-analyzer
-            
-            pkg-config 
-            stdenv.cc 
-            bacon 
-            systemdMinimal 
-          ];
-
-          shellHook = ''
-          export PS1="(esp-rs)$PS1"
-          # this is important - it tells rustup where to find the esp toolchain,
-          # without needing to copy it into your local ~/.rustup/ folder.
-          export RUSTUP_TOOLCHAIN=${esp-rs}
-          '';
-      };
-    };
+    outputs = _: {
+    flakeModule = import ./flake-module.nix;
   };
 }
